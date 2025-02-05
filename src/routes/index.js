9ff7b081456bad7cd79bbd0425c4fc405f47a009
sharon <sharon329@naver.com>
2025-02-05 23:47:12 +0900

    aboutPage/aboutStore

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
+// 프로젝트에서 관리하는 페이지들을 정리하는 페이지
 import { createRouter } from "../core/ronleeda";
 import Home from './Home.js';
 import Movie from './Movie.js';
+import About from './About.js';
 
 export default createRouter([
   { path: '#/', component: Home },
-  { path: '#/movie', component: Movie }
+  { path: '#/movie', component: Movie },
+  { path: '#/about', component: About }
 ])
